76cabf3ca60dc9937480b37e02d00a1a0a601344
nictruong <nic.truong@gmail.com>
2017-05-04 15:31:10 -0400

    finished socket.io tutorial, going to do extras

--- index.js
+++ index.js
@@ -15,14 +15,15 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   	console.log('a user connected');
+  	
+	socket.on('chat message', (msg) => {
+		console.log('message: ' + msg);
+		io.emit('chat message', msg);
+	});
 
-	  	socket.on('chat message', function(msg){
-			console.log('message: ' + msg);
-		});
-
-		socket.on('disconnect', function(){
-			console.log('user disconnected');
-		});
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
 });
 
 
